9134cb331f2abed73bb0fb523616857089b57b81
riptoc <riptoc@protonmail.com>
2019-08-05 00:41:18 +0100

    Changed theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -86,7 +86,7 @@ local themes = {
     "vertex",          -- 10
 }
 
-local chosen_theme = themes[6]
+local chosen_theme = themes[5]
 local modkey       = "Mod4"
 local altkey       = "Mod1"
 local terminal     = "urxvtc"
