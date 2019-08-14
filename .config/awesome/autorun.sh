b34855b305c846acba11f92ae45f3d2fa9f10ec7
riptoc <riptoc@protonmail.com>
2019-08-14 03:39:48 +0100

    Wibar colours changed

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -13,7 +13,7 @@ smart_run() {
 # Default programs
 smart_run firefox
 xbindkeys
-compton --blur-background --blur-method kawase --blur-strength 5 --opacity-rule 85:'class_g="kitty"' --backend glx --shadow --shadow-opacity=1 --clear-shadow --shadow-radius=7 --shadow-offset-x=-10 --shadow-offset-y=-10
+compton --blur-background --blur-method kawase --blur-strength 5 --opacity-rule 85:'class_g="kitty"' --backend glx # --shadow --shadow-opacity=1 --clear-shadow --shadow-radius=7 --shadow-offset-x=-10 --shadow-offset-y=-10
 vpn c
 # Custom scripts
 remapcaps
