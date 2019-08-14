9d59b3d9c8b96892eaa2978360dc289025348491
riptoc <riptoc@protonmail.com>
2019-08-14 17:32:37 +0100

    Added cd/ls function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -4,13 +4,26 @@
 
 # Print path in a nicer format
 PATHs() {
-    printf '%s$PATH:%s\n' "$(tput bold)" "$(tput sgr0)"
-    echo "$PATH" | tr ':' '\n'
+  printf '%s$PATH:%s\n' "$(tput bold)" "$(tput sgr0)"
+  echo "$PATH" | tr ':' '\n'
 }
 
 
 # Create a new directory and enter it
-mc () {
+mc() {
   mkdir -p "$@" && cd "$@"
 }
+
+
+# cd and ls
+cl() {
+  local dir="$1"
+  local dir="${dir:=$HOME}"
+  if [[ -d "$dir" ]]; then
+    cd "$dir" >/dev/null; ls
+  else
+    echo "bash: cl: $dir: Directory not found"
+  fi
+}
+
 
