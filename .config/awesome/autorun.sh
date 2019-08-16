7982ae5bb67b9d0e70aa7cbb7795eb6af7b2a064
riptoc <riptoc@protonmail.com>
2019-08-17 00:12:08 +0100

    Cleaned up autorun, added vs-code to compton

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -13,7 +13,12 @@ smart_run() {
 # Default programs
 smart_run firefox
 xbindkeys
-compton --blur-background --blur-method kawase --blur-strength 5 --opacity-rule 85:'class_g="kitty"' --backend glx # --shadow --shadow-opacity=1 --clear-shadow --shadow-radius=7 --shadow-offset-x=-10 --shadow-offset-y=-10
+compton --blur-background\
+        --blur-method kawase\
+        --blur-strength 5\
+        --opacity-rule 85:'class_g="kitty"'\
+        --opacity-rule 85:'class_g="code-oss"'\
+        --backend glx
 vpn c
 # Custom scripts
 remapcaps
