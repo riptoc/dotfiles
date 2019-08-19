e94021dd2e77c1321296e685e30d36f5bcd082b0
riptoc <riptoc@protonmail.com>
2019-08-19 17:02:26 +0100

    Autorun scripts updated

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -3,7 +3,7 @@
 # Script to be run when awesome starts.
 
 # Run a program if it's not alredy
-smart_run() {
+run() {
   if ! pgrep -f $1 ;
   then
     $@&
@@ -11,16 +11,16 @@ smart_run() {
 }
 
 # Default programs
-smart_run firefox
-xbindkeys
-compton --blur-background\
+run firefox
+run xbindkeys
+run compton --blur-background\
         --blur-method kawase\
         --blur-strength 5\
         --opacity-rule 85:'class_g="kitty"'\
         --opacity-rule 80:'class_g="rofi"'\
         --opacity-rule 85:'class_g="code-oss"'\
         --backend glx
-vpn c
-feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/*
+run vpn c
+run feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/*
 # Custom scripts
-~/bin/remapcaps
+run ~/bin/remapcaps
